915c556b592d59f98b1b25a86989ebad1c97b877
zhangzhihao <464485682@qq.com>
2018-04-22 18:48:10 +0800

    添加七牛云配置

--- server/config/qiniu.js
+++ server/config/qiniu.js
@@ -1,21 +1,36 @@
 const qiniu = require("qiniu");
-//需要填写你的 Access Key 和 Secret Key
-qiniu.conf.ACCESS_KEY = 'Access_Key';
-qiniu.conf.SECRET_KEY = 'Secret_Key';
-//要上传的空间
-bucket = 'album';
+// //需要填写你的 Access Key 和 Secret Key
+var accessKey = 'Access Key';
+var secretKey = 'Secret Key';
+var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+// //要上传的空间
+const bucket = 'album';
+var options = {
+    scope: bucket,
+  };
+var putPolicy = new qiniu.rs.PutPolicy(options);
+var uploadToken = putPolicy.uploadToken(mac);
 
-export default function uploadFile(key) {
-    let token = new qiniu.rs.PutPolicy(bucket+":"+key).token() //生成上传 Token
-    let filePath = `../static/upload/${key}` //要上传文件的本地路径
-    let extra = new qiniu.io.PutExtra();
-    qiniu.io.putFile(token, key, filePath, extra, function(err, ret) {
-        if(!err) {
-          // 上传成功， 处理返回值
-          console.log(ret.hash, ret.key, ret.persistentId);       
-        } else {
-          // 上传失败， 处理返回代码
-          console.log(err);
+
+var config = new qiniu.conf.Config();
+config.zone = qiniu.zone.Zone_z0;
+var formUploader = new qiniu.form_up.FormUploader(config);
+var putExtra = new qiniu.form_up.PutExtra();
+
+
+module.exports = function uploadFile(key) {
+    var localFile = `xxx/${key}`
+    // 文件上传
+    formUploader.putFile(uploadToken, key, localFile, putExtra, function (respErr,
+        respBody, respInfo) {
+        if (respErr) {
+            throw respErr;
         }
-      });
+        if (respInfo.statusCode == 200) {
+            console.log(respBody);
+        } else {
+            console.log(respInfo.statusCode);
+            console.log(respBody);
+        }
+    });
 }
