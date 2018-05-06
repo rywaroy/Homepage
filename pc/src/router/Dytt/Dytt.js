1872723ad4938f725af680b63ca28ec3a2196ec2
zhangzhihao <464485682@qq.com>
2018-05-06 22:03:17 +0800

    Pc电影天堂 eslint

--- pc/src/router/Dytt/Dytt.js
+++ pc/src/router/Dytt/Dytt.js
@@ -1,52 +1,45 @@
-import React, {Component} from 'react';
-import {Row, Col} from 'antd';
-import {observer} from 'mobx-react'
-import store from '../../store'
-import utils from '../../utils'
-import './dytt.css'
+import React, { Component } from 'react';
+import { Row, Col } from 'antd';
+import { observer } from 'mobx-react';
+import store from '../../store';
+import utils from '../../utils';
+import './dytt.css';
 
 @observer
-export default class Dytt extends Component{
+export default class Dytt extends Component {
 
-    constructor(props){
-		super(props)
-		this.state = {
-			
-		  }
-    }
+	componentDidMount() {
+		store.dytt.data.length === 0 && this.getInfo();
+	}
 
-    componentDidMount(){
-        store.dytt.data.length === 0 && this.getInfo()
-    }
-    
-    //获取数据
-    getInfo(){
-		store.loading.show()
-        utils.axios.get('dytt/all',{
-			params:{
-				page:1,
-			}
+	// 获取数据
+	getInfo() {
+		store.loading.show();
+		utils.axios.get('dytt/all', {
+			params: {
+				page: 1,
+			},
 		}).then(res => {
-			store.dytt.setData(res.data.data)
-			store.loading.hide()
-		})
-	}
-	
-	//
-	linkInfo(data){
-		store.dytt.setInfo(data)
-		this.props.history.push(`/movie/dytt/info`)
+			store.dytt.setData(res.data.data);
+			store.loading.hide();
+		});
 	}
 
-	render(){
-		return(
+	// 链接到详情页
+	linkInfo(data) {
+		store.dytt.setInfo(data);
+		this.props.history.push(`/movie/dytt/info`);
+	}
+
+	render() {
+		return (
 			<Row>
 				<Col span={2}/>
 				<Col span={20}>
 					<div className="dytt">
 						{
-							store.dytt.data.map((item,index) => (
-								<div className="dytt-item" key={index} onClick={() => {this.linkInfo(item.content)}}>
+							store.dytt.data.map((item, index) => (
+								<div className="dytt-item" key={index} onClick={() => { this.linkInfo(item.content); }}>
 									<div className="dytt-top f-cb">
 										<div className="dytt-name fl">{item.title}</div>
 										<div className="dytt-time fr">{item.time}</div>
@@ -57,9 +50,9 @@ export default class Dytt extends Component{
 						}
 					</div>
 				</Col>
-				<Col span={2}/>
+				<Col span={2} />
 			</Row>
-		)
+		);
 	}
 }
 
--- pc/src/router/Dytt/Info.js
+++ pc/src/router/Dytt/Info.js
@@ -1,20 +1,20 @@
-import React, {Component} from 'react';
-import {Row, Col} from 'antd';
-import {observer} from 'mobx-react'
-import store from '../../store'
+import React, { Component } from 'react';
+import { Row, Col } from 'antd';
+import { observer } from 'mobx-react';
+import store from '../../store';
 
 @observer
-export default class DyttInfo extends Component{
-	render(){
-		return(
+export default class DyttInfo extends Component {
+	render() {
+		return (
 			<Row>
-				<Col span={2}/>
+				<Col span={2} />
 				<Col span={20}>
-                    <div className="single-page-content" dangerouslySetInnerHTML={{__html: store.dytt.info}}></div>
+					<div className="single-page-content" dangerouslySetInnerHTML={{ __html: store.dytt.info }}></div>
 				</Col>
-				<Col span={2}/>
+				<Col span={2} />
 			</Row>
-		)
+		);
 	}
 }
 
