57ea28979b173c769cfb311d91fdff8e0f75ef02
zhangzhihao <464485682@qq.com>
2018-05-11 23:57:26 +0800

    eslint

--- pc/src/router/Learn/Info.js
+++ pc/src/router/Learn/Info.js
@@ -1,50 +1,47 @@
-import React, {Component} from 'react';
-import {Row, Col} from 'antd';
-import utils from '../../utils'
-import './learn.css'
-import './markdown.css'
-export default class Info extends Component{
+import React, { Component } from 'react';
+import { Row, Col } from 'antd';
+import utils from '../../utils';
+import './learn.css';
+import './markdown.css';
 
-	constructor(props){
-        super(props)
-        this.state = {
-            title:'',
-            html:'',
-        }
-    }
-    
-    componentDidMount(){
-        this.getInfo()
-    }
+export default class Info extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			content: '',
+		};
+	}
 
-    //获取详情
-    getInfo(){
-        utils.axios.get('learn/info',{
-			params:{
-				id:this.props.match.params.id,
-			}
+	componentDidMount() {
+		this.getInfo();
+	}
+
+	// 获取详情
+	getInfo() {
+		utils.axios.get('learn/info', {
+			params: {
+				id: this.props.match.params.id,
+			},
 		}).then(res => {
-            let data = res.data.data
-            this.setState({
-                title:data.title,
-                content:data.html
-            })
-        })
-    }
-	
-	render(){
-		return(
+			const data = res.data.data;
+			this.setState({
+				content: data.html,
+			});
+		});
+	}
+
+	render() {
+		return (
 			<Row>
-				<Col span={2}/>
+				<Col span={2} />
 				<Col span={20}>
-                    <div className="single-page">
-                        <div className="single-page-title">{this.state.title}</div>
-                        <div className="single-page-content markdown-body" dangerouslySetInnerHTML={{__html: this.state.content}}></div>
-                    </div>
+					<div className="single-page">
+						<div className="single-page-content markdown-body" dangerouslySetInnerHTML={{ __html: this.state.content }}></div>
+					</div>
 				</Col>
-				<Col span={2}/>
+				<Col span={2} />
 			</Row>
-		)
+		);
 	}
 }
 
--- pc/src/router/Learn/Learn.js
+++ pc/src/router/Learn/Learn.js
@@ -1,86 +1,82 @@
-import React, {Component} from 'react';
-import {Row, Col, Pagination} from 'antd';
-import utils from '../../utils'
-import store from '../../store'
-import {observer} from 'mobx-react'
-import moment from 'moment'
-import './learn.css'
+import React, { Component } from 'react';
+import { Row, Col, Pagination } from 'antd';
+import { observer } from 'mobx-react';
+import moment from 'moment';
+import utils from '../../utils';
+import store from '../../store';
+import './learn.css';
 
 @observer
-export default class Learn extends Component{
+export default class Learn extends Component {
+	componentDidMount() {
+		store.learn.list.length === 0 && this.getList();
+	}
 
-    componentDidMount(){
-        store.learn.list.length === 0 && this.getList()
-    }
-    
-    //获取文章列表
-    getList(){
-        store.loading.show()
-        utils.axios.get('learn/list',{
-            params:{
-                page:store.learn.page,
-                limit:store.learn.limit,               
-            }
-        }).then(res => {
-                let data = res.data.data
-                store.learn.setList(data.list)
-                store.learn.setTotal(data.total)
-                setTimeout(() => {
-                    store.loading.hide()
-                },1000)
-                
-        })
-    }
+	// 获取文章列表
+	getList() {
+		store.loading.show();
+		utils.axios.get('learn/list', {
+			params: {
+				page: store.learn.page,
+				limit: store.learn.limit,
+			},
+		}).then(res => {
+			const data = res.data.data;
+			store.learn.setList(data.list);
+			store.learn.setTotal(data.total);
+			setTimeout(() => {
+				store.loading.hide();
+			}, 1000);
+		});
+	}
 
-    //页面切换
-    _onChange(page){
-        store.learn.setPage(page)
-        document.documentElement.scrollTop = document.body.scrollTop = 0
-        this.getList()
-    }
+	// 页面切换
+	_onChange(page) {
+		store.learn.setPage(page)
+		document.documentElement.scrollTop = document.body.scrollTop = 0;
+		this.getList()
+	}
 
-    //跳转详情
-    linkInfo(id){
-        this.props.history.push(`/blog/learn/info/` + id)
-    }
-	
-	render(){
-		return(
+	// 跳转详情
+	linkInfo(id) {
+		this.props.history.push(`/blog/learn/info/` + id);
+	}
+
+	render() {
+		return (
 			<Row>
 				<Col span={2}/>
 				<Col span={20}>
-                    <div className="learn">
+					<div className="learn">
 						{
-							store.learn.list.map((item,index) => (
-								<div className="learn-item" key={index} onClick={() => {this.linkInfo(item.id)}}>
+							store.learn.list.map((item, index) => (
+								<div className="learn-item" key={index} onClick={() => { this.linkInfo(item.id); }}>
 									<div className="learn-top f-cb">
 										<div className="learn-name fl">{item.title}</div>
 										<div className="learn-time fr">{moment(item.time).format('YYYY-MM-DD')}</div>
-                                        <div className="learn-tag fr">{item.tag}</div>
+										<div className="learn-tag fr">{item.tag}</div>
 									</div>
 									<div className="learn-content">{item.intro}</div>
 								</div>
 							))
-                        }
-                        {
-                            store.learn.total / store.learn.limit <= 1 ?
-                                null
-                                :
-                                <div className="learn-page">
-                                    <Pagination 
-                                        current={store.learn.page}
-                                        total={store.learn.total}
-                                        pageSize={store.learn.limit}
-                                        onChange={this._onChange.bind(this)}/>
-                                </div>
-                        }
-                        
-                        
+						}
+						{
+							store.learn.total / store.learn.limit <= 1 ?
+								null
+								:
+								<div className="learn-page">
+									<Pagination
+										current={store.learn.page}
+										total={store.learn.total}
+										pageSize={store.learn.limit}
+										onChange={this._onChange.bind(this)} />
+								</div>
+						}
 					</div>
 				</Col>
-				<Col span={2}/>
+				<Col span={2} />
 			</Row>
-		)
+		);
 	}
 }
 
