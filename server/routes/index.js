72864381f9bf884c271439a3284b31aec35ee162
zhangzhihao <464485682@qq.com>
2018-06-04 16:50:45 +0800

    增加 说说模块

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const router = require('koa-router')();
 const article = require('./article');
 const learn = require('./learn')
@@ -9,6 +10,7 @@ const douban = require('./douban');
 const dytt = require('./dytt');
 const weather = require('./weather');
 const base = require('./base');
+const think = require('./think');
 
 router.use('/api/article', article.routes(), article.allowedMethods());
 router.use('/api/learn', learn.routes(), learn.allowedMethods());
@@ -20,6 +22,7 @@ router.use('/api/douban', douban.routes(), douban.allowedMethods());
 router.use('/api/dytt', dytt.routes(), dytt.allowedMethods());
 router.use('/api/weather', weather.routes(), weather.allowedMethods());
 router.use('/api/base', base.routes(), base.allowedMethods());
+router.use('/api/think', think.routes(), think.allowedMethods());
 
 router.get('/admin', async (ctx) => {
   var htmlFile = await (new Promise(function (resolve, reject) {
