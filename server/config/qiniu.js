47c77b8c2ba2a9467d9ab58763197decfa1614ab
zhangzhihao <464485682@qq.com>
2018-06-21 13:39:56 +0800

    封装七牛云图片上传，修改为Promise

--- server/config/qiniu.js
+++ server/config/qiniu.js
@@ -1,36 +1,39 @@
-const qiniu = require("qiniu");
+const qiniu = require('qiniu');
 // //需要填写你的 Access Key 和 Secret Key
-var accessKey = 'Access Key';
-var secretKey = 'Secret Key';
-var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+const accessKey = 'Access Key';
+const secretKey = 'Secret Key';
+const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
 // //要上传的空间
 const bucket = 'album';
-var options = {
-    scope: bucket,
-  };
-var putPolicy = new qiniu.rs.PutPolicy(options);
-var uploadToken = putPolicy.uploadToken(mac);
+const options = {
+	scope: bucket,
+};
+const putPolicy = new qiniu.rs.PutPolicy(options);
+const uploadToken = putPolicy.uploadToken(mac);
 
 
-var config = new qiniu.conf.Config();
+const config = new qiniu.conf.Config();
 config.zone = qiniu.zone.Zone_z0;
-var formUploader = new qiniu.form_up.FormUploader(config);
-var putExtra = new qiniu.form_up.PutExtra();
+const formUploader = new qiniu.form_up.FormUploader(config);
+const putExtra = new qiniu.form_up.PutExtra();
 
 
 module.exports = function uploadFile(key) {
-    var localFile = `xxx/${key}`
-    // 文件上传
-    formUploader.putFile(uploadToken, key, localFile, putExtra, function (respErr,
-        respBody, respInfo) {
-        if (respErr) {
-            throw respErr;
-        }
-        if (respInfo.statusCode == 200) {
-            console.log(respBody);
-        } else {
-            console.log(respInfo.statusCode);
-            console.log(respBody);
-        }
-    });
-}
+	const localFile = `xxx/${key}`;
+	// 文件上传
+	return new Promise ((resolve, reject) => {
+		formUploader.putFile(uploadToken, key, localFile, putExtra, function (respErr, respBody, respInfo) {
+			if (respErr) {
+				throw respErr;
+			}
+			if (respInfo.statusCode === 200) {
+				console.log(respBody);
+				resolve();
+			} else {
+				console.log(respInfo.statusCode);
+				console.log(respBody);
+				reject();
+			}
+		});
+	});
+};
--- server/routes/file.js
+++ server/routes/file.js
@@ -1,35 +1,42 @@
 const router = require('koa-router')();
 const multer = require('koa-multer');
-const uploadQiniu = require('../config/qiniu')
+const uploadQiniu = require('../config/qiniu');
 
-const storage = multer.diskStorage({  
-    //文件保存路径  
-    destination: function (req, file, cb) {  
-      cb(null, 'static/upload/')  
-    },  
-    //修改文件名称  
-    filename: function (req, file, cb) {  
-      var fileFormat = (file.originalname).split(".");  
-      cb(null,Date.now() + "." + fileFormat[fileFormat.length - 1]);  
-    }  
-  })  
-  //加载配置  
-  const upload = multer({ storage: storage });  
+const storage = multer.diskStorage({
+  // 文件保存路径
+  destination(req, file, cb) {
+    cb(null, 'static/upload/');
+  },
+  // 修改文件名称
+  filename(req, file, cb) {
+    const fileFormat = (file.originalname).split('.');
+    cb(null, Date.now() + '.' + fileFormat[fileFormat.length - 1]);
+  },
+});
+// 加载配置
+const upload = multer({
+  storage,
+});
 
 
-  router.post('/photos', upload.single('photos'), async (ctx, next) => { 
-    uploadQiniu(ctx.req.file.filename)
-    // ctx.success('1','上传成功',{filename: 'http://' + ctx.request.host + '/upload/' +ctx.req.file.filename}) 
-    ctx.success('1','上传成功',{filename: 'http://p7kx9r1er.bkt.clouddn.com/' +ctx.req.file.filename}) 
-  })  
+router.post('/photos', upload.single('photos'), async (ctx, next) => {
+  try {
+    await uploadQiniu(ctx.req.file.filename);
+    ctx.success('0000', '上传成功', {
+      filename: `http://p7kx9r1er.bkt.clouddn.com/${ctx.req.file.filename}`,
+    });
+  } catch (err) {
+		ctx.error('0011', '上传失败');
+	}
+});
 
 //   router.post('/photos', upload.single('photos',9), async (ctx, next) => {
 //     let filename = []
 //     for(let i in ctx.req.file.filename){
 //       filename.push('http://' + ctx.request.host + '/upload/' +ctx.req.file.filename[i])
-//     } 
-//     ctx.success('1','上传成功',{filename: filename}) 
-//   }) 
+//     }
+//     ctx.success('1','上传成功',{filename: filename})
+//   })
 
 
-  module.exports = router;
+module.exports = router;
