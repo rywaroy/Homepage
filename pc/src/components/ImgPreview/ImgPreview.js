384fb3f089c8db6ab04a5cf9a472af1321a84be5
zhangzhihao <464485682@qq.com>
2018-06-30 21:14:29 +0800

    图片预览组件增加左右图片切换，关闭功能

--- pc/src/components/ImgPreview/ImgPreview.js
+++ pc/src/components/ImgPreview/ImgPreview.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 import './imgpreview.css';
 
 export default class ImgPreview extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			index: this.props.index,
+		};
+		this.len = this.props.imgs.length;
+	}
+
+	prev() { // 上一张
+		if (this.state.index <= 0) {
+			return;
+		}
+		this.setState({ index: this.state.index - 1 });
+	}
+
+	next() { // 下一张
+		if (this.state.index >= this.len - 1) {
+			return;
+		}
+		this.setState({ index: this.state.index + 1 });
+	}
+
 	render() {
 		return (
 			<div className="img-preview">
-				<img src={this.props.imgs[this.props.index].url} alt="" className="img-preview-item" />
+				<div className="img-preview-close" onClick={() => this.props.close()}></div>
+				<div className="img-preview-left" onClick={() => this.prev()}></div>
+				<img src={this.props.imgs[this.state.index].url} alt="" className="img-preview-item" />
+				<div className="img-preview-right" onClick={() => this.next()}></div>
 			</div>
 		);
 	}
