429a1b17147fa55f9f7add1e08ad6daf24d9f899
zhangzhihao <464485682@qq.com>
2018-07-18 11:46:59 +0800

    增加打卡接口、日期查询接口

--- server/routes/plan.js
+++ server/routes/plan.js
@@ -2,6 +2,7 @@ const router = require('koa-router')();
 const db = require('../database');
 const login = require('../middlewares/isLogin');
 
+// 获取计划列表
 router.get('/list', async ctx => {
   const data = await (new Promise((resolve, reject) => {
     db.query('select id, title from plan where state = 1', function (err, rows) {
@@ -17,6 +18,7 @@ router.get('/list', async ctx => {
 	});
 });
 
+// 新增计划
 router.post('/list', login.isLogin, async ctx => {
   const title = ctx.request.body.title;
   try {
@@ -35,6 +37,7 @@ router.post('/list', login.isLogin, async ctx => {
   }
 });
 
+// 删除计划
 router.post('/list/delete', login.isLogin, async ctx => {
   const id = ctx.request.body.id;
   try {
@@ -51,6 +54,46 @@ router.post('/list/delete', login.isLogin, async ctx => {
   } catch (err) {
     ctx.error('0011', '删除失败');
   }
+});
+
+// 打卡
+router.post('/clock', login.isLogin, async ctx => {
+  const time = ctx.request.body.time;
+  const id = ctx.request.body.id;
+  try {
+    await (new Promise((resolve, reject) => {
+      db.query('insert into plan_record (tid, time) values(?,?)', [id, time], function (err, rows) {
+        if (rows.insertId) {
+					resolve();
+				} else {
+					reject(err);
+				}
+      });
+    }));
+    ctx.success('0000', '打卡成功');
+  } catch (err) {
+    ctx.error('0011', '打卡失败');
+  }
+});
+
+// 查询某日打卡信息
+router.get('/date', async ctx => {
+  const start = ctx.query.start;
+  const end = ctx.query.end;
+  console.log(start);
+  const query = `select time, tid from plan_record where ${start ? `time >= '${start}'` : ''} ${end ? `and time <= '${end}'` : ''}`;
+  const data = await (new Promise((resolve, reject) => {
+    db.query(query, function (err, rows) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  }));
+  ctx.success('0000', '获取成功', {
+		list: data,
+	});
 });
 
 module.exports = router;
