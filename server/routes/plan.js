fac2f29eac8696aaedaa26e36c56aa94fc9f48ed
zhangzhihao <464485682@qq.com>
2018-07-19 16:50:16 +0800

    日期查询 增加tid查询字段， 增加计划查询打卡记录

--- server/routes/plan.js
+++ server/routes/plan.js
@@ -5,7 +5,7 @@ const login = require('../middlewares/isLogin');
 // 获取计划列表
 router.get('/list', async ctx => {
   const data = await (new Promise((resolve, reject) => {
-    db.query('select id, title from plan where state = 1', function (err, rows) {
+    db.query('select id, title from plan where state = 1', (err, rows) => {
       if (err) {
 				reject(err);
 			} else {
@@ -23,7 +23,7 @@ router.post('/list', login.isLogin, async ctx => {
   const title = ctx.request.body.title;
   try {
     await (new Promise((resolve, reject) => {
-      db.query('insert into plan (title) values(?)', [title], function (err, rows) {
+      db.query('insert into plan (title) values(?)', [title], (err, rows) => {
         if (rows.insertId) {
 					resolve();
 				} else {
@@ -42,7 +42,7 @@ router.post('/list/delete', login.isLogin, async ctx => {
   const id = ctx.request.body.id;
   try {
     await (new Promise((resolve, reject) => {
-      db.query('update plan set state = 0 where id = ?', [id], function (err) {
+      db.query('update plan set state = 0 where id = ?', [id], err => {
         if (err) {
 					reject(err);
 				} else {
@@ -62,7 +62,7 @@ router.post('/clock', login.isLogin, async ctx => {
   const id = ctx.request.body.id;
   try {
     await (new Promise((resolve, reject) => {
-      db.query('insert into plan_record (tid, time) values(?,?)', [id, time], function (err, rows) {
+      db.query('insert into plan_record (tid, time) values(?,?)', [id, time], (err, rows) => {
         if (rows.insertId) {
 					resolve();
 				} else {
@@ -80,10 +80,27 @@ router.post('/clock', login.isLogin, async ctx => {
 router.get('/date', async ctx => {
   const start = ctx.query.start;
   const end = ctx.query.end;
-  console.log(start);
-  const query = `select time, tid from plan_record where ${start ? `time >= '${start}'` : ''} ${end ? `and time <= '${end}'` : ''}`;
+  const id = ctx.query.id;
+  const query = `select a.time, a.tid, b.title from plan_record as a left join plan as b on a.tid = b.id where ${start ? `time >= '${start}'` : ''} ${end ? `and time <= '${end}'` : ''} ${id ? `and tid = ${id}` : ''}`;
   const data = await (new Promise((resolve, reject) => {
-    db.query(query, function (err, rows) {
+    db.query(query, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  }));
+  ctx.success('0000', '获取成功', {
+		list: data,
+	});
+});
+
+// 查询某计划打卡记录
+router.get('/plan', async ctx => {
+  const id = ctx.query.id;
+  const data = await (new Promise((resolve, reject) => {
+    db.query('select * from plan_record where tid = ?', [id], (err, rows) => {
       if (err) {
         reject(err);
       } else {
