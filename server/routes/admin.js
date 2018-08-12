ef88d68791e125cee2f271e278e1967c72971de8
zhangzhihao <464485682@qq.com>
2018-08-12 23:11:40 +0800

    Add 上次登录时间和登录地点

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')();
 const db = require('../database');
 const uuid = require('uuid');
+const request = require('request');
 
 router.post('/login', async (ctx) => {
 	const account = ctx.request.body.account;
@@ -10,6 +11,19 @@ router.post('/login', async (ctx) => {
 	if (data.length === 1) {
 		const token = await setToken(account, password);
 		data[0].token = token;
+		const rs = await getIpInfo(ctx.request.header['x-forward-for']);
+		const rd = JSON.parse(rs).data;
+		const location = rd.country + rd.region + rd.city;
+		const time = new Date();
+		await (new Promise((resolve, reject) => {
+			db.query('update admin set last_location = ?, last_time = ?, location = ?, time = ?', [data[0].location, data[0].time, location, time], (err) => {
+				if (err) {
+					reject();
+				} else {
+					resolve();
+				}
+			});
+		}));
 		ctx.success('0000', '登录成功', data[0]);
 	} else {
 		ctx.error('0011', '账户或密码错误');
@@ -36,6 +50,18 @@ function setToken(account, password) {
 				reject(err);
 			} else {
 				resolve(token);
+			}
+		});
+	});
+}
+
+function getIpInfo(ip) {
+	return new Promise((resolve, reject) => {
+		request.get(`http://ip.taobao.com/service/getIpInfo.php?ip=${ip}`, function (error, response, body) {
+			if (error) {
+				reject(error);
+			} else {
+				resolve(body);
 			}
 		});
 	});
