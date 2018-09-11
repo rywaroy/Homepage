8bca7151127386101f44251a327ba765e07d14fc
zhangzhihao <464485682@qq.com>
2018-09-11 18:22:47 +0800

    update 使用sequelize重构

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,59 +1,76 @@
 const router = require('koa-router')();
-const db = require('../database');
 const uuid = require('uuid');
 const request = require('request');
+const sequelize = require('../sequelize');
+const Sequelize = require('sequelize');
+
+const Admin = sequelize.define('admin', {
+	account: {
+		type: Sequelize.STRING,
+	},
+	password: {
+		type: Sequelize.STRING,
+	},
+	token: {
+		type: Sequelize.STRING,
+	},
+	name: {
+		type: Sequelize.STRING,
+	},
+	avatar: {
+		type: Sequelize.STRING,
+	},
+	time: {
+		type: Sequelize.DATE,
+	},
+	last_time: {
+		type: Sequelize.DATE,
+	},
+	location: {
+		type: Sequelize.STRING,
+	},
+	last_location: {
+		type: Sequelize.STRING,
+	},
+});
 
 router.post('/login', async (ctx) => {
 	const account = ctx.request.body.account;
 	const password = ctx.request.body.password;
-	const data = await findAdmin(account, password);
-
-	if (data.length === 1) {
-		const token = await setToken(account, password);
-		data[0].token = token;
-		const rs = await getIpInfo(ctx.request.header['x-forward-for']);
-		const rd = JSON.parse(rs).data;
-		const location = rd.country + rd.region + rd.city;
-		const time = new Date();
-		await (new Promise((resolve, reject) => {
-			db.query('update admin set last_location = ?, last_time = ?, location = ?, time = ?', [data[0].location, data[0].time, location, time], (err) => {
-				if (err) {
-					reject();
-				} else {
-					resolve();
-				}
-			});
-		}));
-		ctx.success('0000', '登录成功', data[0]);
-	} else {
-		ctx.error('0011', '账户或密码错误');
+	if (!account || !password) {
+		ctx.error(500, '请输入账号或密码');
+		return;
 	}
+
+	const data = await Admin.findOne({
+		where: {
+			account,
+			password,
+		},
+	});
+	if (!data) {
+		ctx.error(500, '账号或密码错误');
+		return;
+	}
+	const rs = await getIpInfo(ctx.request.header['x-forward-for']);
+	const rd = JSON.parse(rs).data;
+	const location = rd.country + rd.region + rd.city;
+	const time = new Date();
+	const token = uuid.v4();
+	await Admin.update({
+		token,
+		last_location: data.location,
+		last_time: data.time,
+		location,
+		time,
+	}, {
+		where: {
+			id: data.id,
+		},
+	});
+	data.token = token;
+	ctx.success('0000', '登录成功', data);
 });
-
-function findAdmin(account, password) {
-	return new Promise(function (resolve, reject) {
-		db.query('select * from admin where account = "' + account + '" and password = "' + password + '"', function (err, rows) {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(rows);
-			}
-		});
-	});
-}
-
-function setToken(account, password) {
-	return new Promise(function (resolve, reject) {
-		const token = uuid.v4();
-		db.query('update admin set token = "' + token + '" where account = "' + account + '" and password = "' + password + '"', function (err) {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(token);
-			}
-		});
-	});
-}
 
 function getIpInfo(ip) {
 	return new Promise((resolve, reject) => {
