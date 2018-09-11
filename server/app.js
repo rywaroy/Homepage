66b10dbd5f99b8bc0d5df76f5aa6473825b5e41a
zhangzhihao <464485682@qq.com>
2018-09-11 18:21:59 +0800

    Add onerror

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const convert = require('koa-convert');
 const json = require('koa-json');
 const bodyparser = require('koa-bodyparser')();
 const staticServer = require('koa-static');
+const onerror = require('koa-onerror');
 
 // 中间件
 app.use(staticServer(`${__dirname}/static`));
@@ -17,6 +18,7 @@ app.use(convert(json()));
 app.use(require('./middlewares/returnData'));
 
 app.use(router.routes(), router.allowedMethods());
+onerror(app);
 
 app.listen(3001);
 // console.log('app started at port 3001...');
