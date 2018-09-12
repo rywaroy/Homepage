3a4d691075323d148f528b6c90dcfd3ecfa2f156
zhangzhihao <464485682@qq.com>
2018-09-12 16:26:00 +0800

    Update admin api

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,44 +1,13 @@
 const router = require('koa-router')();
 const uuid = require('uuid');
 const request = require('request');
-const sequelize = require('../sequelize');
-const Sequelize = require('sequelize');
-
-const Admin = sequelize.define('admin', {
-	account: {
-		type: Sequelize.STRING,
-	},
-	password: {
-		type: Sequelize.STRING,
-	},
-	token: {
-		type: Sequelize.STRING,
-	},
-	name: {
-		type: Sequelize.STRING,
-	},
-	avatar: {
-		type: Sequelize.STRING,
-	},
-	time: {
-		type: Sequelize.DATE,
-	},
-	last_time: {
-		type: Sequelize.DATE,
-	},
-	location: {
-		type: Sequelize.STRING,
-	},
-	last_location: {
-		type: Sequelize.STRING,
-	},
-});
+const Admin = require('../model/admin');
 
 router.post('/login', async (ctx) => {
 	const account = ctx.request.body.account;
 	const password = ctx.request.body.password;
 	if (!account || !password) {
-		ctx.error(500, '请输入账号或密码');
+		ctx.error(400, '请输入账号或密码');
 		return;
 	}
 
@@ -49,7 +18,7 @@ router.post('/login', async (ctx) => {
 		},
 	});
 	if (!data) {
-		ctx.error(500, '账号或密码错误');
+		ctx.error(400, '账号或密码错误');
 		return;
 	}
 	const rs = await getIpInfo(ctx.request.header['x-forward-for']);
@@ -69,7 +38,7 @@ router.post('/login', async (ctx) => {
 		},
 	});
 	data.token = token;
-	ctx.success('0000', '登录成功', data);
+	ctx.success(200, '登录成功', data);
 });
 
 function getIpInfo(ip) {
