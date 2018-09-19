ce61babbbb05b4fefd85162ebc4b269fc41ad259
zhangzhihao <464485682@qq.com>
2018-09-19 09:42:11 +0800

    Update think api

--- server/routes/think.js
+++ server/routes/think.js
@@ -1,32 +1,22 @@
 const router = require('koa-router')();
-const db = require('../database');
 const login = require('../middlewares/isLogin');
+const Think = require('../model/think');
+const Admin = require('../model/admin');
 
 // 获取说说列表
 router.get('/list', async ctx => {
-  const page = ctx.query.page || '1';
-  const limit = ctx.query.limit || '10';
-  const data = await (new Promise((resolve, reject) => {
-    db.query('select * from think where state = 1 order by time desc limit ' + (page - 1) * limit + ' , ' + limit, function (err, rows) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(rows);
-      }
-    });
-  }));
+  const page = ctx.query.page || 1;
+  const limit = ctx.query.limit || 10;
+  const data = await Think.findAndCount({
+    where: {
+      state: 1,
+    },
+    order: [['time', 'DESC']],
+    limit,
+		offset: (page - 1) * limit,
+  });
 
-  const count = await (new Promise((resolve, reject) => {
-    db.query('select count(*) as count from think  where state = 1', function (err, rows) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(rows);
-      }
-    });
-  }));
-
-  for (let item of data) {
+  for (const item of data.rows) {
     if (item.photos) {
       item.photos = item.photos.split(',');
     } else {
@@ -34,31 +24,25 @@ router.get('/list', async ctx => {
     }
   }
 
-  ctx.success('0000', '获取成功', {
-    list: data,
-    count: count[0].count,
+  ctx.success(200, '获取成功', {
+    list: data.rows,
+    count: data.count,
   });
 });
 
 // 获取说说详情
 router.get('/info', async ctx => {
   const id = ctx.query.id;
-  const data = await (new Promise((resolve, reject) => {
-    db.query('select * from think  where id = ' + id, function (err, rows) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(rows);
-      }
-    });
-  }));
-  if (data[0].photos) {
-    data[0].photos = data[0].photos.split(',');
+
+  const data = await Think.findById(id);
+
+  if (data.photos) {
+    data.photos = data.photos.split(',');
   } else {
-    data[0].photos = [];
+    data.photos = [];
   }
 
-  ctx.success('0000', '获取成功', data[0]);
+  ctx.success(200, '获取成功', data);
 });
 
 
@@ -68,48 +52,30 @@ router.post('/info', login.isLogin, async ctx => {
   const photos = ctx.request.body.photos;
   const time = new Date();
 
-  try {
-    const user = await (new Promise((resolve, reject) => {
-      db.query('select name, avatar from admin where account = "admin"', function (err, row) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(row);
-        }
-      });
-    }));
-    await (new Promise((resolve, reject) => {
-      db.query('insert into think (avatar,name,time,content,photos) values (?,?,?,?,?)', [user[0].avatar, user[0].name, time, content, photos], (err, rows) => {
-        if (rows.insertId) {
-          resolve();
-        } else {
-          reject(err);
-        }
-      });
-    }));
-    ctx.success('0000', '添加成功');
-  } catch (err) {
-    ctx.error('0011', '添加失败');
-  }
+  const user = await Admin.findById(1);
+
+  await Think.create({
+    avatar: user.avatar,
+    name: user.name,
+    time,
+    content,
+    photos,
+  });
+
+  ctx.success(200, '添加成功');
 });
 
 // 删除说说
 router.post('/delete', login.isLogin, async ctx => {
   const id = ctx.request.body.id;
-  try {
-    await (new Promise((resolve, reject) => {
-      db.query('update think set state = 0 where id = ?', [id], function (err, row) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    }));
-    ctx.success('0000', '删除成功');
-  } catch (err) {
-    ctx.error('0011', '删除失败');
-  }
+  await Think.update({
+    state: 0,
+  }, {
+    where: {
+      id,
+    },
+  });
+  ctx.success(200, '删除成功');
 });
 
 // 修改说说
@@ -117,20 +83,15 @@ router.post('/edit', login.isLogin, async ctx => {
   const id = ctx.request.body.id;
   const content = ctx.request.body.content;
   const photos = ctx.request.body.photos;
-  try {
-    await (new Promise((resolve, reject) => {
-      db.query('update think set content = ? , photos = ? where id = ?', [content, photos, id], function (err) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    }));
-    ctx.success('0000', '修改成功');
-  } catch (err) {
-    ctx.error('0011', '修改失败');
-  }
+  await Think.update({
+    content,
+    photos,
+  }, {
+    where: {
+      id,
+    },
+  });
+  ctx.success(200, '修改成功');
 });
 
 module.exports = router;
