742201a37ba708316d7591576c3b665ae043aa46
zhangzhihao <464485682@qq.com>
2018-11-16 18:01:43 +0800

    Fix bug

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 // import * as Koa from 'koa';
+import router from './router';
 const Koa = require('koa');
 const cors = require('koa-cors');
 const convert = require('koa-convert');
@@ -9,12 +10,12 @@ const json = require('koa-json');
 const app = new Koa();
 
 app.use(cors());
-app.use(convert(bodyparser({
+app.use(bodyparser({
   formLimit: '5mb',
-})));
+}));
 app.use(staticServer(`${__dirname}/static`));
-app.use(convert(bodyparser));
 app.use(convert(json()));
 app.use(require('./middlewares/returnData'));
+app.use(router.routes(), router.allowedMethods());
 
 app.listen(3001);
