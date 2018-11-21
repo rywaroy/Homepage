4211f99f8b022aa97b7ecab11cb9ef19b655e803
zhangzhihao <464485682@qq.com>
2018-11-21 23:44:51 +0800

    Add: admin interface

---
+++ interface/admin.ts
@@ -0,0 +1,12 @@
+export interface IAdmin {
+  id: number
+  account: string
+  password: string
+  token: string
+  name: string
+  avatar: string
+  time: string
+  last_time: string
+  location: string
+  last_location: string
+}
