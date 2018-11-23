4a788dc310a8d0099fd46a7aabd917c66bf2f4bd
zhangzhihao <464485682@qq.com>
2018-11-23 17:46:47 +0800

    add: group api

--- router/album.ts
+++ router/album.ts
@@ -3,11 +3,11 @@ import isLogin from '../middlewares/isLogin';
 import Album from '../model/album';
 import AlbumGroup from '../model/album_group';
 import IContext from '../interface/context';
-import { IAlbum } from '../interface/album';
+import { IAlbum, IGroup } from '../interface/album';
 
 const router: Router = new Router();
 
-router.post('/album', isLogin, async (ctx: IContext) => {
+router.post('/', isLogin, async (ctx: IContext) => {
 	const url: string = ctx.request.body.url;
 	const title: string = ctx.request.body.title;
 	const time: object = new Date();
@@ -19,7 +19,7 @@ router.post('/album', isLogin, async (ctx: IContext) => {
 	ctx.success(200, '添加成功');
 });
 
-router.delete('/album/:id', isLogin, async (ctx: IContext) => {
+router.delete('/:id', isLogin, async (ctx: IContext) => {
 	const id = ctx.params.id;
 	await Album.update({
 		state: 0,
@@ -31,10 +31,40 @@ router.delete('/album/:id', isLogin, async (ctx: IContext) => {
 	ctx.success(200, '删除成功');
 });
 
-router.get('/album', async (ctx: IContext) => {
+router.get('/', async (ctx: IContext) => {
 	const data: IAlbum[] = await Album.findAll({
 		where: {
 			state: 1,
+		},
+	});
+	ctx.success(200, '获取成功', data);
+});
+
+router.post('/:id/group', isLogin, async (ctx: IContext) => {
+	const url: string = ctx.request.body.url;
+	const id: number = ctx.params.id;
+	await AlbumGroup.create({
+		url,
+		album_id: id,
+	});
+	ctx.success(200, '添加成功');
+});
+
+router.delete('/group/:id', isLogin, async (ctx: IContext) => {
+	const id: number = ctx.params.id;
+	await AlbumGroup.destroy({
+		where: {
+			id,
+		},
+	});
+	ctx.success(200, '删除成功');
+});
+
+router.get('/:id/group', async (ctx: IContext) => {
+	const id: number = ctx.params.id;
+	const data: IGroup[] = await AlbumGroup.findAll({
+		where: {
+			album_id: id,
 		},
 	});
 	ctx.success(200, '获取成功', data);
