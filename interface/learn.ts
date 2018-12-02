336f35ae76b64de8e16cd2973c6668e6d334e714
zhangzhihao <464485682@qq.com>
2018-12-02 19:51:34 +0800

    add: interface learn

---
+++ interface/learn.ts
@@ -0,0 +1,24 @@
+export interface IList {
+  id: number
+  title: string
+  time: string
+  intro: string
+  tag: string
+}
+
+export interface ICount {
+  rows: IList[]
+  count: number
+}
+
+export interface IInfo {
+  id: number
+  title: string
+  update: string
+  intro: string
+  state: number
+  md: string
+  html: string
+  tag: string
+  type: number
+}
