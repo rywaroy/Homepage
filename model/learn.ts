7b026e9b766c270280cb4308eee0f9891d70c4c0
zhangzhihao <464485682@qq.com>
2018-12-02 19:51:41 +0800

    Add: model learn

---
+++ model/learn.ts
@@ -0,0 +1,36 @@
+import * as Sequelize from 'sequelize';
+import sequelize from '../config/sequelize';
+
+const Learn = sequelize.define('learn', {
+  title: {
+    type: Sequelize.TEXT,
+  },
+  time: {
+    type: Sequelize.DATE,
+  },
+  update: {
+    type: Sequelize.DATE,
+  },
+  intro: {
+    type: Sequelize.TEXT,
+  },
+  state: {
+    type: Sequelize.INTEGER,
+    defaultValue: 1,
+  },
+  md: {
+    type: Sequelize.TEXT,
+  },
+  html: {
+    type: Sequelize.TEXT,
+  },
+  tag: {
+    type: Sequelize.TEXT,
+  },
+  type: {
+    type: Sequelize.INTEGER,
+    defaultValue: 1,
+  },
+});
+
+export default Learn;
