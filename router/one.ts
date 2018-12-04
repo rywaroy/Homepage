ba7849127edcf2ee188e0c6ee96c2a2bac9a4766
zhangzhihao <464485682@qq.com>
2018-12-04 16:50:37 +0800

    add: one api

---
+++ router/one.ts
@@ -0,0 +1,42 @@
+import * as Router from 'koa-router';
+import * as request from 'request';
+import IContext from '../interface/context';
+
+const router: Router = new Router();
+
+// 列表
+router.get('/list', async (ctx: IContext) => {
+  const date: string = ctx.query.date;
+  const data: any = await (new Promise((resolve) => {
+		request(`http://v3.wufazhuce.com:8000/api/channel/one/${date}/%E6%9D%AD%E5%B7%9E%E5%B8%82`, function (error, response, body) {
+			resolve(JSON.parse(body));
+		});
+  }));
+	ctx.success(200, '获取成功', data.data);
+});
+
+// 连载详情
+router.get('/info', async (ctx: IContext) => {
+  const id: number = ctx.query.id;
+  const type: number = ctx.query.type;
+  const data: any = await (new Promise((resolve) => {
+		request(`http://v3.wufazhuce.com:8000/api/${type}/htmlcontent/${id}`, function (error, response, body) {
+			resolve(JSON.parse(body));
+		});
+  }));
+	ctx.success(200, '获取成功', data.data);
+});
+
+// 评论
+router.get('/comment', async (ctx: IContext) => {
+  const id: number = ctx.query.id;
+  const type: number = ctx.query.type;
+  const data: any = await (new Promise((resolve) => {
+		request(`http://v3.wufazhuce.com:8000/api/comment/praiseandtime/${type}/${id}/0`, function (error, response, body) {
+			resolve(JSON.parse(body));
+		});
+	}));
+	ctx.success(200, '获取成功', data.data);
+});
+
+export default router;
