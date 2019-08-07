9c216693e868109a105e70d73ac7a017521faafb
张志豪 <464485682@qq.com>
2019-08-07 11:08:20 +0800

    Fix

--- router/douban.ts
+++ router/douban.ts
@@ -21,7 +21,7 @@ router.get('/list', async (ctx: IContext) => {
 	const city: string = ctx.query.city || '杭州';
 	const start: number = (page - 1) * limit;
 	options.url = `https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b&city=${city}&start=${start}&count=${limit}&client=&udid=`;
-	const data = await (new Promise((resolve) => {
+	const data: any = await (new Promise((resolve) => {
 		request(options, function (error, response, body) {
 			resolve(JSON.parse(body));
 		});
@@ -31,7 +31,7 @@ router.get('/list', async (ctx: IContext) => {
 
 router.get('/info', async (ctx: IContext) => {
 	const id: number = ctx.query.id;
-	const data = await (new Promise((resolve) => {
+	const data: any = await (new Promise((resolve) => {
 		options.url = `http://api.douban.com/v2/movie/subject/${id}?apikey=0b2bdeda43b5688921839c8ecb20399b&city=&client=&udid=`;
 		request(options, function (error, response, body) {
 			// console.log(response)
@@ -47,7 +47,7 @@ router.get('/image', async (ctx) => {
 	const limit: number = ctx.query.limit;
 	const start: number = (page - 1) * limit;
 	const count: number = page * limit;
-	const data = await (new Promise((resolve) => {
+	const data: any = await (new Promise((resolve) => {
 		options.url = `http://api.douban.com/v2/movie/subject/${id}/photos?apikey=0b2bdeda43b5688921839c8ecb20399b&start=${start}&count=${count}&client=&udid=`;
 		request(options, function (error, response, body) {
 			// console.log(response)
@@ -62,7 +62,7 @@ router.get('/comment', async (ctx) => {
 	const page: number = ctx.query.page;
 	const limit: number = ctx.query.limit;
 	const start: number = (page - 1) * limit;
-	const data = await (new Promise((resolve) => {
+	const data: any = await (new Promise((resolve) => {
 		options.url = `http://api.douban.com/v2/movie/subject/${id}/comments?apikey=0b2bdeda43b5688921839c8ecb20399b&start=${start}&count=${limit}&client=&udid=`;
 		request(options, function (error, response, body) {
 			// console.log(response)
